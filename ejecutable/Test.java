b3ada5a8b660e6faad0daff05ec98b800e5d7272
Maria2214220 <102614834+Maria2214220@users.noreply.github.com>
2022-10-15 09:36:16 -0500

    siuuuuuuu

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -1,24 +1,15 @@
 package ejecutable;
 
-import modelo.vendedores;
+import controlador.Controlador;
+import modelo.Empleado;
+import modelo.EmpresaAutomotriz;
+import vista.VentanaPrincipal;
 
-public class Test
-{
-    public static void main(String[] args) 
-    {
-        vendedores Vendedor1 = new vendedores("Yo", 2, 1000);
-        System.out.println("Nombre del vendedor:" + Vendedor1.getNombre());
-        System.out.println("Salario minimo: " + vendedores.getSalarioMinimo());
-        System.out.println("Nomina Mensual: " + Vendedor1.getnominaMensual());
-        System.out.println("Numero de carros vendidos: " + Vendedor1.getnumCarrosVendidos());
 
-        System.out.println(Vendedor1);
-
-        System.out.println("------------------------------");
-
-        vendedores Vendedor2 = new vendedores("Fernanda" , 2, 1000000);
-        Vendedor2.nomina_mensual("Fernanda ", 2, 1000000);
-        System.out.println(Vendedor2);
-
-    }   
+public class Test {
+    public static void main(String[] args) {
+        EmpresaAutomotriz miVenderCarros = new EmpresaAutomotriz();
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        new Controlador(miVentana, miVenderCarros);
+    }
 }
